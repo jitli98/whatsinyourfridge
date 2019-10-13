0ff1240b0eb8d8d772a4f783735bb85438ea078d
Jit Gan <jitli98@live.com>
2019-10-13 09:41:39 -0700

    Finalize recipe controller

--- FridgeBackEnd/controllers/recipeController.js
+++ FridgeBackEnd/controllers/recipeController.js
@@ -6,20 +6,23 @@ const Ingredient = require('./../models/ingredientModel');
 // the json file of recipes to the client
 exports.getRecipes = async (req, res) => {
     try {
-        // const ingredients = await Ingredient.find();
-        const ingredients = ['sugar', 'apples', 'flour'];
+        const ingredients = await Ingredient.find();
+        query_param = [];
+        ingredients.forEach(ig => query_param.push(ig.name.toLowerCase()));
+
+        // const query_param = ['sugar', 'apples', 'flour'];
         let url = 'https://api.spoonacular.com/recipes/findByIngredients?'
         url += 'apiKey=5b11632d08f14c3ca1620bc70757f427';
-        if (ingredients.length != 0) {
-            url += '&ingredients='
-            if (ingredients.length >= 1) {
-                url += ingredients[0];
+        if (query_param.length != 0) {
+            url += '&query_param='
+            if (query_param.length >= 1) {
+                url += query_param[0];
             }
-            ingredients.slice(1).forEach(ig => url += `,+${ig}`);
+            query_param.slice(1).forEach(ig => url += `,+${ig}`);
         } 
         const response = await fetch(url);
         const json = await response.json();
-        console.log(json);
+
         res.status(200).json({
             status: 'success',
             data: json
