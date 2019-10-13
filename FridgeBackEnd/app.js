22bb8517b8ca6a3cba73e07dda014e6ad4010d9d
Jit Gan <jitli98@live.com>
2019-10-13 00:23:32 -0700

    Added image post functionality

--- FridgeBackEnd/app.js
+++ FridgeBackEnd/app.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const ingredientController = require('./controllers/ingredientController');
+const fridgeImageController = require('./controllers/fridgeImageController');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const multer = require('multer');
+
 dotenv.config({path: './config.env'});
+
 const app = express();
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, './uploads/');
+    },
+    filename: function(req, file, cb) {
+        cb(null, file.originalname);
+    }
+});
+const upload = multer({storage: storage});
 
 // #2 Middleware stuff
 app.use(express.json());
@@ -11,6 +24,7 @@ app.use(express.json());
 // #3 Routing stuff
 app.get('/ingredient', ingredientController.getIngredients);
 app.post('/ingredient', ingredientController.postIngredients);
+app.post('/fridge-image', upload.single('fridgeImage'), fridgeImageController.postImage);
 
 /// #4 Connecting to database
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
